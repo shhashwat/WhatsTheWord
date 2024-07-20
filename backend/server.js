d8aa4745ac526e94cf00ed2f185bd5d1985c1b21
Shashwat <sshashwatssingh@gmail.com>
2024-07-21 02:18:49 +0530

    1- post routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import {v2 as cloudinary} from 'cloudinary';
 
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
+import postRoutes from './routes/post.routes.js';
 
 import connectMongoDB from './db/connectMongoDB.js';
 
@@ -24,6 +25,7 @@ app.use(cookieParser())
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/post', postRoutes);
 
 connectMongoDB().then(() => {
     app.listen(PORT, () => {
