32ce672452cc79f8dabf790cb0fa9c815e35eed6
Shashwat <sshashwatssingh@gmail.com>
2024-07-23 12:05:57 +0530

    3- added commenting fn

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ cloudinary.config({
 const app = express({limit: "5mb"});
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //to parse the request body as a JSON object
+app.use(express.json({limit: "5mb"})); //to parse the request body as a JSON object
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); //to parse from data(urlencoded)
 app.use(cookieParser())
--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,11 +8,18 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {toast} from 'react-hot-toast';
 import LoadingSpinner from "../common/LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const { data: authUser } = useQuery({queryKey: ['authUser']});
 	const queryClient = useQueryClient();
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authUser._id);
+
+	const isMyPost = authUser._id === post.user._id;
+
+	const formattedDate = formatPostDate(post.createdAt);
 	
 	const {mutate: deletePost, isPending: isDeleting} = useMutation({
 		mutationFn: async () => {
@@ -49,8 +56,6 @@ const Post = ({ post }) => {
 			}
 		},
 		onSuccess: (updatedLikes) => {
-			toast.success("Post liked successfully");
-
 			//not the best UX
 			/*queryClient.invalidateQueries({ queryKey: ["posts"] });*/
 
@@ -70,14 +75,33 @@ const Post = ({ post }) => {
 		},
 	})
 
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser._id);
+	const {mutate: commentPost, isPending: isCommenting} = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/comment/${post._id}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ text: comment }),
+				})
 
-	const isMyPost = authUser._id === post.user._id;
-
-	const formattedDate = "1h";
-
-	const isCommenting = false;
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Failed to comment post");
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Comment posted successfully");
+			setComment("");
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		}
+	})
 
 	const handleDeletePost = () => {
 		deletePost();
@@ -85,6 +109,8 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if(isCommenting) return;
+		commentPost();
 	};
 
 	const handleLikePost = () => {
