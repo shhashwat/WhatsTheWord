a7ab8fc5b13a7d6601292198ebfb7eb744602195
Shashwat <sshashwatssingh@gmail.com>
2024-07-23 17:19:40 +0530

    redid homepage a bit

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
 	const [feedType, setFeedType] = useState("forYou");
 
 	return (
-		<>
+		<>	
 			<div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
 				{/* Header */}
 				<div className='flex w-full border-b border-gray-700'>
