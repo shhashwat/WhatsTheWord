a3b6df39c5345b0b07c16d47033cd4cdb23605a7
Shashwat <sshashwatssingh@gmail.com>
2024-07-23 13:28:52 +0530

    3 - added a delete all notifications functionality

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -30,25 +30,3 @@ export const deleteNotifications = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error >_<" });
     }
 }
-
-export const deleteOneNotification = async (req, res) => {
-    try {
-        const notificationId = req.parms.id;
-        const userId = req.user._id;
-
-        const notification = await Notification.findById(notificationId);
-        if(!notification) return res.status(404).json({error: "Notification not found"});
-
-
-        if(notification.to.toString() !== userId.toString()){
-            return res.status(403).json({error: "You are not authorised to delete this notification"});
-        }
-
-        await Notification.findByIdAndDelete(notificationId);
-
-        res.status(200).json({message: "Notification deleted successfully"});
-    } catch (error) {
-        console.log("Error in deleteOneNotification controller", error.message);
-        res.status(500).json({ error: "Internal Server Error >_<" });
-    }
-}
