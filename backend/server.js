92f97a176ab02ee3bca5a0fb206f8637c839fc99
Shashwat <sshashwatssingh@gmail.com>
2024-07-23 17:40:22 +0530

    4 - prepared the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import {v2 as cloudinary} from 'cloudinary';
@@ -19,6 +20,7 @@ cloudinary.config({
 
 const app = express({limit: "5mb"});
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "5mb"})); //to parse the request body as a JSON object
 // limit shouldn't be too high to prevent DOS
@@ -29,6 +31,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 connectMongoDB().then(() => {
     app.listen(PORT, () => {
